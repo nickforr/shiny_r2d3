f44e0cbc579b238fe4a52ac461da9df100c38840
Nick Forrester <nickforr@me.com>
2019-01-26 13:30:53 +0000

    axis start 0

--- create_dummy_line_data.R
+++ create_dummy_line_data.R
@@ -5,8 +5,10 @@ create_dummy_line_data <- function(n, sigma) {
   
   tt <- 15
   ref <- paste0("ref_", purrr::flatten_dbl(purrr::map(seq_len(n), ~rep.int(.x, 15))))
-  x <- rep.int(seq_len(tt), n)
-  y <- purrr::flatten_dbl(purrr::map(seq_len(n), ~0.7 * cumprod(1 + rnorm(tt, 0.02, sigma))))
+  x <- rep.int(seq_len(tt), n) - 1
+  y <- 
+    purrr::flatten_dbl(
+      purrr::map(seq_len(n), ~0.7 * c(1, cumprod(1 + rnorm(tt - 1, 0.02, sigma)))))
   tibble::tibble(
     ref = ref, 
     xval = x, 
