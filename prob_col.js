4a1e57b74a06a1cbd2e5aad6a439804031e04554
Nick Forrester <nickforr@me.com>
2019-01-27 20:48:33 +0000

    rename

--- prob_col.js
+++ prob_col.js
@@ -1,4 +1,4 @@
-// !preview r2d3 data=data.frame(y = c(5000,2000,3000,4000), x = c(1,2,4,5), label = c('jan', 'feb', 'mar', 'apr'))
+// !preview r2d3 data=data.frame(y = c(5000,2000,3000,4000), x = c(1,2,4,5), label = c('jan', 'feb', 'mar', 'apr'), ylabel = prettyNum(c(5000,2000,3000,4000), big.mark = ','))
 
 var layer_left   = 0.01;
     layer_top    = 0.2;
@@ -89,7 +89,7 @@ totals.enter().append('text')
       .attr('text-anchor', 'middle')
       .style('font-size', '10px') 
       .style('font-family', 'sans-serif')
-      .text(function(d) {return d.y; });  
+      .text(function(d) {return d.ylabel; });  
       
 totals.exit().remove();
 
@@ -97,7 +97,7 @@ totals.transition()
   .duration(500)
       .attr('x', function(d, i) {return (i * col_width()) + (svg_width()* layer_left) + (col_width() * 0.5); })
       .attr('y', function(d) {return col_top() + ((actual_max() - d.y) * col_heigth()); })
-      .text(function(d) {return d.y; });
+      .text(function(d) {return d.ylabel; });
 
 // Title
 svg.append('text')
