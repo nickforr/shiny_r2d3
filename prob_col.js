c5a3730f84af5585e34c2d1bb0ca257ee6c3f95d
Nick Forrester <nickforr@me.com>
2019-01-27 22:11:19 +0000

    format layout

--- prob_col.js
+++ prob_col.js
@@ -1,15 +1,15 @@
-// !preview r2d3 data=data.frame(y = c(5000,2000,3000,4000), x = c(1,2,4,5), label = c('jan', 'feb', 'mar', 'apr'), ylabel = prettyNum(c(5000,2000,3000,4000), big.mark = ','))
+// !preview r2d3 data=data.frame(y = c(70,80,60,20), x = c(1,2,4,5), label = c('jan', 'feb', 'mar', 'apr'), ylabel = sprintf("%.f%%", c(70,80,60,20)))
 
-var layer_left   = 0.01;
-    layer_top    = 0.2;
-    layer_height = 0.7;
-    layer_width  = 0.97;
+var layer_left   = 0.1;
+    layer_top    = 0.1;
+    layer_height = 0.8;
+    layer_width  = 0.85;
 
 function svg_height() {return parseInt(svg.style('height'))}
 function svg_width() {return parseInt(svg.style('width'))}
-function actual_max() {return d3.max(data, function (d) {return d.y; }); }
+function actual_max() {return 100; }
 function col_width()  {return svg_width()  / data.length * layer_width;}
-function col_heigth() {return (svg_height() /actual_max()) * layer_height; }
+function col_height() {return (svg_height() / actual_max()) * layer_height; }
 
 function col_top()  {return svg_height() * layer_top; }
 function col_left() {return svg_width()  * layer_left;}
@@ -17,10 +17,10 @@ function col_left() {return svg_width()  * layer_left;}
 var cols = svg.selectAll('rect').data(data);
 
 cols.enter().append('rect')
-  .attr('height', function(d) {return (d.y * col_heigth()); })
+  .attr('height', function(d) {return (d.y * col_height()); })
   .attr('width', col_width())
   .attr('x', function(d, i) {return (i * col_width()) + (svg_width()* layer_left); })
-  .attr('y', function(d) {return col_top() + ((actual_max() - d.y) * col_heigth()); })
+  .attr('y', function(d) {return col_top() + ((actual_max() - d.y) * col_height()); })
   .attr('fill', '#009E73')
   .attr('opacity', 0.5)
   .attr('stroke', 'white')
@@ -47,10 +47,10 @@ cols.exit().remove();
 
 cols.transition()
   .duration(500)
-  .attr('height', function(d) {return (d.y * col_heigth()); })
+  .attr('height', function(d) {return (d.y * col_height()); })
   .attr('width', col_width())
   .attr('x', function(d, i) {return (i * col_width()) + (svg_width()* layer_left); })
-  .attr('y', function(d) {return col_top() + ((actual_max() - d.y) * col_heigth()); })
+  .attr('y', function(d) {return col_top() + ((actual_max() - d.y) * col_height()); })
   .attr('fill', '#009E73')
   .attr('opacity', 0.5)
   .attr('stroke', 'white');
@@ -85,7 +85,7 @@ var totals = svg.selectAll('totals').data(data);
 
 totals.enter().append('text')
       .attr('x', function(d, i) {return (i * col_width()) + (svg_width()* layer_left) + (col_width() * 0.5); })
-      .attr('y', function(d) {return (col_top() * 0.9) + ((actual_max() - d.y) * col_heigth()); })
+      .attr('y', function(d) {return (col_top() * 0.9) + ((actual_max() - d.y) * col_height()); })
       .attr('text-anchor', 'middle')
       .style('font-size', '10px') 
       .style('font-family', 'sans-serif')
@@ -96,22 +96,26 @@ totals.exit().remove();
 totals.transition()
   .duration(500)
       .attr('x', function(d, i) {return (i * col_width()) + (svg_width()* layer_left) + (col_width() * 0.5); })
-      .attr('y', function(d) {return col_top() + ((actual_max() - d.y) * col_heigth()); })
+      .attr('y', function(d) {return col_top() + ((actual_max() - d.y) * col_height()); })
       .text(function(d) {return d.ylabel; });
 
 // Title
 svg.append('text')
   .attr('x', svg_width()* 0.01)             
   .attr('y', svg_height()* 0.05)
-  .style('font-size', '18px') 
+  .style('font-size', '16px') 
   .style('font-family', 'sans-serif')
-  .text('Total flights');
-  
-//Sub-title
-svg.append('text')
-  .attr('x', svg_width()* 0.99)             
-  .attr('y', svg_height()* 0.05)
-  .attr('text-anchor', 'end')
-  .style('font-size', '12px') 
-  .style('font-family', 'sans-serif')
-  .text('Click bar for details');
+  .text('Probability of success');
+ 
+// y axis...
+var y = d3.scaleLinear()
+    .domain([0,100]).nice()
+    .range([actual_max() * col_height(), 0]);
+
+var y_axis = d3.axisLeft()
+			        .scale(y);
+
+var yaxis = svg.append('g')
+      .attr("class", "y axis")
+      .attr("transform", function(d) {return "translate(" +  svg_width() * layer_left * 0.9 + "," + col_top() + ")"; })
+      .call(y_axis);
